85f66c36a3a1765a40a47f6ce6e3f3aee5d7d658
diogogoulart <71290366+diogogoulart@users.noreply.github.com>
2023-05-02 16:17:04 +0100

    Update GeneticOperators.py

--- stdgp/GeneticOperators.py
+++ stdgp/GeneticOperators.py
@@ -91,17 +91,18 @@ import random
 	return [second_tournament(Sp) for _ in selected]'''
 
 
-def double_tournament(rng, population, Sf, Sp, tournament_size ,switch=False, custom_fitness=None):
-	best = None
-	for _ in range(tournament_size):
-		competitor = rng.choice(population)
-		print(f"Competitor: {competitor_}")  # Debugging
-		competitor_fitness = competitor.size
-		print(f"Competitor size: {competitor_fitness}")  # Debugging
-		if best is None or competitor_fitness < best[1]:
-			best = (competitor, competitor_fitness)
-			print(f"New best: {best}")  # Debugging
-	return best[0]
+def parsimony_tournament(rng, population, tournament_size):
+    best = None
+    for _ in range(tournament_size):
+        competitor = rng.choice(population)
+        print(f"Competitor: {competitor}")  # Debugging
+        competitor_fitness = competitor.size
+        print(f"Competitor size: {competitor_fitness}")  # Debugging
+        if best is None or competitor_fitness < best[1]:
+            best = (competitor, competitor_fitness)
+            print(f"New best: {best}")  # Debugging
+    return best[0]
+
 
 def tournament(rng, population, tournament_size, custom_fitness=None):
 	best = None

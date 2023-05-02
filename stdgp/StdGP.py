f330a593aac9b8bddf958dc6e811dbae1a493d4c
diogogoulart <71290366+diogogoulart@users.noreply.github.com>
2023-05-02 14:31:31 +0100

    Update StdGP.py

--- stdgp/StdGP.py
+++ stdgp/StdGP.py
@@ -320,7 +320,7 @@ class StdGP:
 		newPopulation.extend(getElite(self.population, self.elitism_size))
 		while len(newPopulation) < self.population_size:
 			#offspring = double_tournament(self.population, self.Sf, self.Sp, self.switch)
-			offspring = double_tournament(self.population, self.Sf, self.Sp, self.population_size, self.switch)
+			offspring = double_tournament(self.population, self.Sf, self.Sp, self.tournament_size, self.switch)
 			offspring = discardDeep(offspring, self.max_depth)
 			newPopulation.extend(offspring)
 		self.population = newPopulation[:self.population_size]

11a13b7024d3ad51710cc5e34d6305e40fadf305
diogogoulart <71290366+diogogoulart@users.noreply.github.com>
2023-05-02 19:14:38 +0100

    Update StdGP.py

--- stdgp/StdGP.py
+++ stdgp/StdGP.py
@@ -1,5 +1,5 @@
 from .Individual import Individual
-from .GeneticOperators import getElite, getOffspring, discardDeep, parsimony_tournament
+from .GeneticOperators import getElite, getOffspring, discardDeep
 import multiprocessing as mp
 import time
 
@@ -71,13 +71,12 @@ class StdGP:
 
 
 
-	def __init__(self, operators=[("+",2),("-",2),("*",2),("/",2)], max_initial_depth = 6, population_size = 500,
+	def __init__(self, operators=[("+",2),("-",2),("*",2),("/",2)], max_initial_depth = 6, population_size = 500, 
 		max_generation = 100, tournament_size = 5, elitism_size = 1, max_depth = 17, 
-		threads=1, random_state = 42, verbose = True, model_name="SimpleThresholdClassifier", fitnessType="Accuracy",
-		Sf=7, Sp=3, switch=False):
+		threads=1, random_state = 42, verbose = True, model_name="SimpleThresholdClassifier", fitnessType="Accuracy"):
 
-		if sum([0 if op in [("+",2),("-",2),("*",2),("/",2)] else 0 for op in operators]) > 0:
-			print("[Warning] Some of the following operators may not be supported:", operators)
+		if sum( [0 if op in [("+",2),("-",2),("*",2),("/",2)] else 0 for op in operators ] ) > 0:
+			print( "[Warning] Some of the following operators may not be supported:", operators)
 
 		self.operators = operators
 
@@ -95,12 +94,7 @@ class StdGP:
 		self.model_name = model_name
 		self.fitnessType = fitnessType
 
-		self.Sf = Sf
-		self.Sp = Sp
-		self.switch = switch
-
 		self.verbose = verbose
-
 
 
 
@@ -317,12 +311,9 @@ class StdGP:
 
 		# Generating Next Generation
 		newPopulation = []
-		##newPopulation.extend(getElite(self.population, self.elitism_size))
-		newPopulation = [self.getBest()]
+		newPopulation.extend(getElite(self.population, self.elitism_size))
 		while len(newPopulation) < self.population_size:
-			#offspring = double_tournament(self.population, self.Sf, self.Sp, self.switch)
-			##offspring = double_tournament(self.population, self.Sf, self.Sp, self.tournament_size, self.switch)
-			offspring = parsimony_tournament(self.rng, self.population, self.tournament_size)
+			offspring = getOffspring(self.rng, self.population, self.tournament_size)
 			offspring = discardDeep(offspring, self.max_depth)
 			newPopulation.extend(offspring)
 		self.population = newPopulation[:self.population_size]
@@ -369,3 +360,6 @@ def fitIndividuals(a):
 
 	
 	return ret 
+
+
+
